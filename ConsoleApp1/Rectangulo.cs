5e5f3ca42ef16c439c672a5e5d7802e0f9417535
SaulLV <saul01lov@gmail.com>
2025-08-20 09:25:54 -0600

    Comit inicial

---
+++ ConsoleApp1/Rectangulo.cs
@@ -0,0 +1,32 @@
+﻿
+
+public class Rectangulo
+{
+    //Atributos (lados)
+
+    public double Base { get; set; }
+    public double Altura { get; set; }
+
+    public Rectangulo(double baseee, double altura)
+    {
+        Base = baseee;
+        Altura = altura;
+
+    }
+    /*Contructor (Valor de lados)
+    double baseee = double.Parse(Console.ReadLine() ?? "");
+    double altura = double.Parse(Console.ReadLine() ?? "");
+    Metodos (Mostrar el perimetro, mostrar area)*/
+    public virtual double Area()
+    {
+        return Base * Altura;
+        /*Console.Write("Area:");
+        Console.WriteLine(area);*/
+    }
+    public virtual double Perimetro()
+    {
+        return (2 * Base) + (2 * Altura);
+        /*Console.Write("Perimetro:");
+        Console.WriteLine(perimetro);*/
+    }
+}
