9fc99039bcc7f7151f3add29b657677996adcba4
CarlosRodriguez112 <cacos12@comunidad.unam.mx>
2025-08-31 18:27:47 -0600

    HOlaTears

---
+++ ConsoleApp1/Dispositivos/Lampara.cs
@@ -0,0 +1,48 @@
+﻿public class Lampara : Dispositivos
+{
+    // Constructor
+    public Lampara(string nombre, int consumo) : base(nombre, consumo)
+    {
+
+    }
+    private int intensidad;
+
+    public void LaClaseBase(string nombre, int consumo, int intensidad)
+    {
+        this.Nombre = nombre;
+        this.AjustarConsumo(consumo);
+        this.intensidad = intensidad;
+    }
+
+    public void AjustarIntensidad(int nuevaIntensidad)
+    {
+        if(Encendido == false)
+        {
+            Console.WriteLine("La lámpara está apagada. No se puede ajustar la intensidad.");
+            return;
+        }
+        else if (Encendido == true)
+        {
+            if (nuevaIntensidad < 0)
+            {
+                nuevaIntensidad = 0;
+            }
+            else if (nuevaIntensidad > 2000)
+            {
+                intensidad = 2000;
+                Console.WriteLine($"La intensidad de {Nombre} se ha ajustado a {intensidad} lm");
+            }
+            else
+            {
+                intensidad = nuevaIntensidad;
+                Console.WriteLine($"La intensidad de {Nombre} se ha ajustado a {intensidad} lm");
+            }
+                
+        }
+            
+    }
+    public override void MostrarInfo()
+    {
+        Console.WriteLine($"Dispositivo: {Nombre}\n Consumo: {Consumo} W\n Intensidad: {intensidad} lm\n Estado: {(Encendido ? "Encendido" : "Apagado")}");
+    }
+}
---
+++ ConsoleApp1/Dispositivos/Princi.cs
@@ -0,0 +1,29 @@
+﻿Lampara lampara1 = new Lampara("Lampara de mesa", 100);
+
+lampara1.Encender();
+lampara1.AjustarConsumo();
+lampara1.AjustarIntensidad(2800);
+lampara1.MostrarInfo();
+
+Console.WriteLine("");
+
+Ventilador ventilador1 = new Ventilador("Ventilador de techo", 100);
+ventilador1.Encender();
+ventilador1.AjustarConsumo(2500);
+ventilador1.AjustarVelocidad(800);
+ventilador1.MostrarInfo();
+
+Console.WriteLine("");
+
+if (ventilador1.Consumo > lampara1.Consumo)
+{
+    Console.WriteLine("El ventilador consume más que la lámpara.");
+}
+else if (ventilador1.Consumo < lampara1.Consumo)
+{
+    Console.WriteLine("La lámpara consume más que el ventilador.");
+}
+else if (ventilador1.Consumo == lampara1.Consumo)
+{
+    Console.WriteLine("Ambos dispositivos consumen lo mismo.");
+}
