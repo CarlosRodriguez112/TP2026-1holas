35fab1bb42d2990b0cb567b1ed5ecc53e9344c14
CarlosRodriguez112 <cacos12@comunidad.unam.mx>
2025-08-31 18:27:47 -0600

    Tears

---
+++ ConsoleApp1/Dispositivos/Dispositivos.cs
@@ -0,0 +1,111 @@
+﻿public class Dispositivos
+{
+    // Atributos 
+    private string nombre;
+    private bool encendido;
+    private int consumo;
+
+
+    // Propiedades publicas con control
+    public string Nombre
+    {
+        get { return nombre; }
+        set { nombre = value; }
+    }
+    public bool Encendido
+    {
+        get { return encendido; }
+        private set { encendido = value; }
+    }
+    public int Consumo
+    {
+        get { return consumo; }
+        private set
+        {
+            if (value < 0)
+            {
+                consumo = 0;
+            }
+            else
+            {
+                consumo = value;
+            }
+        }
+    }
+
+
+    // Constructor
+
+    public Dispositivos(string nombre, int consumo)
+    {
+        this.nombre = nombre;
+        this.consumo = consumo;
+        this.encendido = true;   //Encendido y apagado
+    }
+
+    public void Encender()
+    {
+        if (encendido == true)
+        {
+            Console.WriteLine($"{encendido} está encendido.");
+        }
+        else if (encendido == false)
+        {
+            Console.WriteLine($"{nombre} está apagada.");
+        }
+        
+    }
+    public void AjustarConsumo (int nuevoConsumo)
+    {
+        if (encendido == false)
+        {
+            Console.WriteLine($"No se puede ajustar el consumo mientras {nombre} está apagado.");
+            Consumo = 0;
+        }
+        else if (encendido == true)
+        {
+            if (nuevoConsumo < 0)
+            {
+                Console.WriteLine($"El consumo no puede ser negativo");
+            }
+            else if (nuevoConsumo > 2040)
+            {
+                Consumo = 2040;
+                Console.WriteLine($"El consumo de {nombre} se ha ajustado a {consumo} W.");
+            }
+            else
+            {
+                Consumo = nuevoConsumo;
+                Console.WriteLine($"El consumo de {nombre} se ha ajustado a {consumo} W.");
+            }
+        }
+        
+    }
+
+    public void AjustarConsumo()
+    {
+        Console.WriteLine($"El consumo de {nombre} es de {consumo} W.");
+    }
+
+    public virtual void MostrarInfo()
+    {
+        Console.WriteLine($"Dispositivo: {nombre}, Encendido: {encendido}, Consumo: {consumo} W");
+    }
+
+    public static bool operator > (Dispositivos d1, Dispositivos d2)
+    {
+        return d1.consumo > d2.consumo;
+    }
+    public static bool operator < (Dispositivos d1, Dispositivos d2)
+    {
+        return d1.consumo < d2.consumo;
+    }
+    public static bool operator != (Dispositivos d1, Dispositivos d2)
+    {
+        return d1.consumo != d2.consumo;
+    }
+    public static bool operator == (Dispositivos d1, Dispositivos d2)
+    {
+        return d1.consumo == d2.consumo;
+    }
+}
---
+++ ConsoleApp1/Dispositivos/Ventilador.cs
@@ -0,0 +1,48 @@
+﻿public class Ventilador : Dispositivos
+{
+    // Constructor
+    public Ventilador(string nombre, int consumo) : base(nombre, consumo)
+    {
+
+    }
+    private int Velocidad;
+
+    public void LaClaseBase(string nombre, int consumoL, int velocidad)
+    {
+        this.Nombre = nombre;
+        this.AjustarConsumo(consumoL);
+        this.Velocidad = velocidad;
+    }
+
+    public void AjustarVelocidad(int nuevaVelocidad)
+    {
+        if (Encendido == false)
+        {
+            Console.WriteLine("La lámpara está apagada. No se puede ajustar la intensidad.");
+            return;
+        }
+        else if (Encendido == true)
+        {
+            if (nuevaVelocidad < 0)
+            {
+                nuevaVelocidad = 0;
+            }
+            else if (nuevaVelocidad > 500)
+            {
+                Velocidad = 500;
+                Console.WriteLine($"La velocidad de {Nombre} se ha ajustado a {Velocidad} rad/s");
+            }
+            else
+            {
+                Velocidad = nuevaVelocidad;
+                Console.WriteLine($"La velocidad de {Nombre} se ha ajustado a {Velocidad} rad/s");
+            }
+            
+        }
+        
+    }
+    public override void MostrarInfo()
+    {
+        Console.WriteLine($"Dispositivo: {Nombre}\n Consumo: {Consumo} W\n Velocidad: {Velocidad} rad/s\n Estado: {(Encendido ? "Encendido" : "Apagado")}");
+    }
+}
