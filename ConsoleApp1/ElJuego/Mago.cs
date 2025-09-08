6f4bf03afbf9ffe7f5bd0e0af40d424eb47142c1
Saul <saul01lov@gmail.com>
2025-09-08 10:51:05 -0600

    Siguimiento con el juego GGs

---
+++ ConsoleApp1/ElJuego/Mago.cs
@@ -0,0 +1,13 @@
+﻿public class Mago : Guerrero
+{
+    public Mago(string nombre) : base(nombre, 80, 25)
+    {
+
+    }
+
+    public override void Atacar(Guerrero enemigo)
+    {
+        Console.WriteLine($"{Nombre} (Mago) Lanza un hechizo d fuego {enemigo.Nombre}");
+        base.Atacar(enemigo);
+    }
+}
